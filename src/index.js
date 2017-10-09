15286557f3d5921686d0f39e01105132a26fca93
Cynthia <cynthialouis.dev@gmail.com>
2017-10-09 11:58:21 +0200

    Import css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+// For js files, no need of extension '.js'
 import React from 'react';
 import { render } from 'react-dom';
+//css
+import './style/css/bootstrap.min.css';
+import './index.css';
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Coucou</h1>
+            // html class => use 'className'
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <textarea rows="35" className="form-control" value="Tapez votre texte">
+
+                        </textarea>
+                    </div>
+                    <div className="col-sm-6">
+                        <h1>Resultats</h1>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
